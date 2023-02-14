f8d6b39bdbb1237f286978c6999b8271cc766e07
gabriel-bucalon <gabriel.bucalon@ciandt.com>
2023-02-14 17:11:44 -0300

    feature: added config to styles and doctype

--- src/setupJest.ts
+++ src/setupJest.ts
@@ -1,1 +1,23 @@
 import 'jest-preset-angular/setup-jest';
+
+Object.defineProperty(window, 'CSS', { value: null });
+Object.defineProperty(document, 'doctype', {
+  value: '<!DOCTYPE html>'
+});
+Object.defineProperty(document.body.style, 'transform', {
+  value: () => {
+    return {
+      enumerable: true,
+      configurable: true
+    };
+  }
+});
+
+import type {Config} from 'jest';
+import {defaults} from 'jest-config';
+
+const config: Config = {
+  moduleFileExtensions: [...defaults.moduleFileExtensions, 'mts'],
+};
+
+export default config;
