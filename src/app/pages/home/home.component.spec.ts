f6eb1990ecb0c8ff8439a67288111787b9a76f17
gabriel-bucalon <gabriel.bucalon@ciandt.com>
2023-02-14 17:12:10 -0300

    feature: added a new unit test for Home

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,23 +1,36 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
+import { Router } from '@angular/router';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+  let router: Router;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
+  beforeEach((() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        HomeComponent
+      ],
+      providers: [
+        { provide: Router, useClass: RouterStub }
+      ]
+    });
 
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    component = TestBed.createComponent(HomeComponent).componentInstance;
+    router = TestBed.get(Router);
+  }));
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should navigate to /sign route', () => {
+    const navigateSpy = jest.spyOn(router, 'navigate');
+    component.gotoFromRegister();
+    expect(navigateSpy).toHaveBeenCalledWith(['/sign']);
+  });
 });
+
+class RouterStub {
+  navigate() {}
+}
