f0355e99b6641c8470945dfc2b4b489e1cccc7b0
gabriel-bucalon <gabriel.bucalon@ciandt.com>
2023-02-14 17:19:27 -0300

    feature: create test to my-input

--- src/app/components/my-input/my-input.component.spec.ts
+++ src/app/components/my-input/my-input.component.spec.ts
@@ -1,23 +1,20 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MyInputComponent } from './my-input.component';
 
 describe('MyInputComponent', () => {
   let component: MyInputComponent;
-  let fixture: ComponentFixture<MyInputComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ MyInputComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(MyInputComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => {
+    component = new MyInputComponent();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit data when sendDataToParent is called', () => {
+    const mockData = 'test data';
+    const spy = jest.spyOn(component.sendData, 'emit');
+    component.sendDataToParent({ target: { value: mockData } } as any);
+    expect(spy).toHaveBeenCalledWith(mockData);
   });
 });
