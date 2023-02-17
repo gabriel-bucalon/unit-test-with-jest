d09651dea37d1dea03bcab43b6743e046823bea2
gabriel-bucalon <gabriel.bucalon@ciandt.com>
2023-02-17 17:32:22 -0300

    feat: create new  route to list persons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormSignComponent } from './pages/form-sign/form-sign.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ListEmployeeComponent } from './pages/list-employee/list-employee.component';
 
 const routes: Routes = [
   {
@@ -12,7 +13,11 @@ const routes: Routes = [
   {
     path: "sign",
     component: FormSignComponent
-  }
+  },
+  {
+    path: "list-employee",
+    component: ListEmployeeComponent,
+  },
 ];
 
 @NgModule({
